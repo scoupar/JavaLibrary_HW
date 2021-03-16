6534ac542d5f2c975ca7053f77a1c3c392708e3e
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-03-16 20:03:45 +0000

    bookcount test passing

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,5 +4,12 @@ public class Library {
 
     private ArrayList<Book> books;
 
+    public Library(){
+        this.books = new ArrayList<Book>();
+    }
+
+    public int bookCount(){
+        return this.books.size();
+    }
 
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,14 +1,22 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
 
     private Library library;
     private Book book;
 
-//    @Before
-//    public void before(){
-//        library = new Library();
-//        book = new Book();
+    @Before
+    public void before(){
+        library = new Library();
+        book = new Book("1984", "George Orwell", "Sci-Fi" );
+    }
+
+    @Test
+    public void countNumberOfBooksInLibrary(){
+        assertEquals(0, library.bookCount());
     }
 
 
